7081e8e267347ab19cbf1825d60dffb73cbe424b
Gladi023 <Gladimar.Villamil@gmail.com>
2022-06-22 16:16:08 -0400

    changes made to app.js and page-template.js

--- src/page-template.js
+++ src/page-template.js
@@ -1,4 +1,4 @@
-const generatePage = (name, github) => {
+module.exports = (name, github) => {
 
     return `
     
@@ -21,10 +21,3 @@ const generatePage = (name, github) => {
 
 }; 
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    if (err) throw new Error(err);
-
-    console.log('Portfolio complete! Checkout index.html to see the output!');
-});
-
-module.exports = generatePage;
